c5921d8920065d52f0877d671c7eaf0044cef1cf
NTDCONCUBETI <162284202+NTDCONCUBETI@users.noreply.github.com>
2024-11-15 19:34:47 +0700

    Update AutoFarmMap7ChuongInfinite2Unit.lua

--- Anime/AutoFarmMap7ChuongInfinite2Unit.lua
+++ Anime/AutoFarmMap7ChuongInfinite2Unit.lua
@@ -259,12 +259,9 @@ if game.PlaceId == 17017769292 or game.PlaceId == 17764698696 then
 end
 
 if game.PlaceId == 17018663967 then
-	local function click(a, offsetY)
-		local x = a.AbsolutePosition.X + a.AbsoluteSize.X / 2
-		local y = a.AbsolutePosition.Y + a.AbsoluteSize.Y / 2 + (offsetY or 0)
-		-- Gửi sự kiện nhấn chuột
-		game:GetService("VirtualInputManager"):SendMouseButtonEvent(x, y, 0, true, a, 1)
-		game:GetService("VirtualInputManager"):SendMouseButtonEvent(x, y, 0, false, a, 1)
+	function click(a)
+		game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+a.AbsoluteSize.Y/2+60,0,true,a,1)
+		game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+a.AbsoluteSize.Y/2+60,0,false,a,1)
 	end
 
 	-- Thiết lập camera thành Scriptable để điều khiển bằng script
@@ -306,7 +303,7 @@ if game.PlaceId == 17018663967 then
 		while true do
 			camera.CFrame = cameraCFrame * rotation
 			wait(1)
-			click(tableUnit[i], 60)
+			click( tableUnit[i] )
 			wait(1)
 			tab( x , y )
 			wait(1)
@@ -328,19 +325,9 @@ if game.PlaceId == 17018663967 then
 	function ugradeUnit(x,y)
 		camera.CFrame = cameraCFrame * rotation
 		wait(0.5)
-		wait(1)
 		tab( x , y )
 		wait(1)
-		click(btnUpgrade, 20)
-		wait(1)
-		tab( x , y )
-		wait(1)
-		click(btnUpgrade, 40)
-		wait(1)
-		tab( x , y )
-		wait(1)
-		click(btnUpgrade, 60)
-		wait(1)
+		click(btnUpgrade)
 	end
 
 	spawn(function()
@@ -519,7 +506,7 @@ if game.PlaceId == 17018663967 then
 	playerGui.HUD.LocalUnitHolder.MainFrame.HolderTop.Visible = false ---- Ẩn bảng chỉ số update Unit
 	playerGui.HUD.LocalUnitHolder.MainFrame.ViewportFrame.Visible = false -- Ẩn hình ảnh Unit của bảng Update
 	playerGui.HUD.Toolbar.Visible = false --- Ẩn hàng ngang 6 ô Unit 
-	wait(60)
+	wait(2)
 	------- Update Aether Knight
 	tab(368 , 130)------Tab hiện bảng update
 	wait(1)
